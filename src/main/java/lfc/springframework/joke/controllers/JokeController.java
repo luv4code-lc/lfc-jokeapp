8d05d1a64a41c35919856f8e3dfe4791495dfc5b
luv4boot <luv4boot@gmail.com>
2021-11-18 18:32:13 +0530

    adding Joke Controller

---
+++ src/main/java/lfc/springframework/joke/controllers/JokeController.java
@@ -0,0 +1,21 @@
+package lfc.springframework.joke.controllers;
+
+import lfc.springframework.joke.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class JokeController {
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @GetMapping({"/", ""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
+        return "chucknorris";
+    }
+}
