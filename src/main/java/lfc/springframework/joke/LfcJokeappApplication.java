e1fb26efc47aff1b794930a3c6c5f85a25286587
luv4boot <luv4boot@gmail.com>
2021-11-18 18:08:43 +0530

    initial commit

---
+++ src/main/java/lfc/springframework/joke/LfcJokeappApplication.java
@@ -0,0 +1,13 @@
+package lfc.springframework.joke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LfcJokeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LfcJokeappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lfc/springframework/joke/LfcJokeappApplicationTests.java
@@ -0,0 +1,13 @@
+package lfc.springframework.joke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LfcJokeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
