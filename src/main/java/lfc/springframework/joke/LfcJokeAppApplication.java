1e02b52e32f233de184dd5f85ead5d3480ee4bea
luv4boot <luv4boot@gmail.com>
2021-11-19 11:07:07 +0530

    adding xml based configuration

--- src/main/java/lfc/springframework/joke/LfcJokeAppApplication.java
+++ src/main/java/lfc/springframework/joke/LfcJokeAppApplication.java
@@ -2,8 +2,10 @@ package lfc.springframework.joke;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-norris-config.xml")
 public class LfcJokeAppApplication {
 
     public static void main(String[] args) {
